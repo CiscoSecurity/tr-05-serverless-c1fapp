301c88a43fac15ae09e9f25f3f80acb6a4e72c39
AnastasiiaRozlyvan <48979187+AnastasiiaRozlyvan@users.noreply.github.com>
2020-06-04 20:32:44 +0300

    CCTRI-1169 Query for Sightings (convert data from C1fApp into CTIM) (#4)
    * CCTRI-1169 Query for Sightings (convert data from C1fApp into CTIM)
    
    * fix flake8
    
    * Updates for title and source
    
    * Add Unit Tests
    
    * Add error handling with data
    
    * Fix indent, naming and extra get's

--- api/utils.py
+++ api/utils.py
@@ -1,6 +1,6 @@
 from authlib.jose import jwt
 from authlib.jose.errors import JoseError
-from flask import request, current_app, jsonify
+from flask import request, current_app, jsonify, g
 from api.errors import InvalidJWTError, InvalidArgumentError
 
 
@@ -40,3 +40,27 @@ def jsonify_data(data):
 
 def jsonify_errors(error):
     return jsonify({'errors': [error]})
+
+
+def all_subclasses(cls):
+    """
+    Retrieve set of class subclasses recursively.
+    """
+    subclasses = set(cls.__subclasses__())
+    return subclasses.union(s for c in subclasses for s in all_subclasses(c))
+
+
+def format_docs(docs):
+    return {'count': len(docs), 'docs': docs}
+
+
+def jsonify_result():
+    result = {'data': {}}
+
+    if g.get('sightings'):
+        result['data']['sightings'] = format_docs(g.sightings)
+
+    if g.get('errors'):
+        result['errors'] = g.errors
+
+    return jsonify(result)
