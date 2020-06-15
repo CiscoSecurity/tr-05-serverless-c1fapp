1f4c8011f53871fec3c84ddad18123e7259ebdfe
AnastasiiaRozlyvan <nastia.2.r.2@gmail.com>
2020-06-15 13:49:03 +0300

    [CCTRI-1251] Fix error inconsistencies

--- api/client.py
+++ api/client.py
@@ -4,6 +4,11 @@ from flask import current_app
 
 from api.errors import UnexpectedC1fAppError
 
+NOT_CRITICAL_ERRORS = (
+    'Unsupported request ? IPv4/Domain only',  # ToDo
+    'Empty Search! Availiable search: IPv4/URL/Domain'
+)
+
 
 class C1fAppClient:
     def __init__(self, api_key):
@@ -24,7 +29,10 @@ class C1fAppClient:
             self.api_url, headers=self.headers, json=self.data
         )
 
-        if not response.ok:
-            raise UnexpectedC1fAppError(response)
+        if response.text in NOT_CRITICAL_ERRORS:
+            return []
 
-        return response.json()
+        if response.ok:
+            return response.json()
+
+        raise UnexpectedC1fAppError(response)
